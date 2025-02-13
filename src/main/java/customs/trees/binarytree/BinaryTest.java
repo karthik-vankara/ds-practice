c33e04504fd8f4aa74737a7df239367c0cc3dbeb
Karthik Vankara <vankara.karthik@gmail.com>
2025-02-13 22:25:25 +0530

    Binary tree basics

---
+++ src/main/java/customs/trees/binarytree/BinaryTest.java
@@ -0,0 +1,9 @@
+package customs.trees.binarytree;
+
+public class BinaryTest {
+    public static void main(String[] args) {
+        BinaryTree binaryTree = new BinaryTree();
+        binaryTree.insert();
+        binaryTree.prettyDisplay();
+    }
+}
---
+++ src/main/java/customs/trees/binarytree/BinaryTree.java
@@ -0,0 +1,86 @@
+package customs.trees.binarytree;
+
+import java.util.Scanner;
+
+public class BinaryTree {
+
+    private Node root;
+
+    public BinaryTree() {
+    }
+
+    public static class Node{
+        int value;
+        Node left;
+        Node right;
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public void insert(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter root node: ");
+        int rootValue = scanner.nextInt();
+        root = new Node(rootValue);
+        insertNextNodes(root,scanner);
+    }
+
+    private void insertNextNodes(Node node, Scanner scanner) {
+        System.out.print("Do you want to insert left of "+node.value+ " ??");
+        boolean left = scanner.nextBoolean();
+        if(left){
+            System.out.print("Enter left value: ");
+            int leftVal = scanner.nextInt();
+            Node leftNode = new Node(leftVal);
+            node.left = leftNode;
+            insertNextNodes(leftNode,scanner);
+        }
+
+        System.out.print("Do you want to insert right of "+node.value+" ??");
+        boolean right = scanner.nextBoolean();
+        if(right){
+            System.out.print("Enter right value: ");
+            int rightVal = scanner.nextInt();
+            Node rightNode = new Node(rightVal);
+            node.right = rightNode;
+            insertNextNodes(rightNode,scanner);
+        }
+    }
+
+    public void display(){
+        display(root,"");
+    }
+
+    private void display(Node node, String indent) {
+        if(node == null){
+            return;
+        }
+        System.out.println(indent+node.value);
+        display(node.left,indent + "\t");
+        display(node.right,indent+"\t");
+    }
+
+    public void prettyDisplay(){
+        prettyDisplay(root,0);
+    }
+
+    private void prettyDisplay(Node node, int level) {
+        if(node == null){
+            return;
+        }
+        prettyDisplay(node.right,level+1);
+        if (level != 0) {
+            for (int i = 0; i < level - 1; i++) {
+                System.out.print("|\t\t");
+            }
+            System.out.println("|------->" + node.value);
+        } else {
+            System.out.println(node.value);
+        }
+        prettyDisplay(node.left,level+1);
+
+    }
+
+
+}
