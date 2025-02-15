dbd06edd6c853d7d7abdb8084390a7e193ac4fa9
Karthik Vankara <vankara.karthik@gmail.com>
2025-02-15 16:06:40 +0530

    AVl tree

--- src/main/java/customs/trees/binarytree/AVL.java
+++ src/main/java/customs/trees/binarytree/AVL.java
@@ -1,161 +1,164 @@
 package customs.trees.binarytree;
 
-public class AVL {
+class AVL {
 
-    private Node root;
-
-    public AVL() {
-    }
-
-    public static class Node{
-        int value;
-        Node left;
-        Node right;
-        int height;
+    public class Node {
+        private int value;
+        private Node left;
+        private Node right;
+        private int height;
 
         public Node(int value) {
             this.value = value;
         }
 
-        public int getValue(){
+        public int getValue() {
             return value;
         }
     }
 
-    public int height(){
-        return height(root);
+    private Node root;
+
+    public AVL() {
+
     }
 
-    public int height(Node node){
-        if(node == null){
+    public int height() {
+        return height(root);
+    }
+    private int height(Node node) {
+        if (node == null) {
             return -1;
         }
         return node.height;
     }
 
-    public boolean isEmpty(){
-        return root == null;
+    public void insert(int value) {
+        root = insert(value, root);
     }
 
-    public void pupulate(int[] arr){
-        for (int i = 0; i < arr.length; i++) {
-            insert(arr[i]);
-        }
-    }
-
-    //     if arry is sorted
-    public void pupulateSorted(int[] arr){
-        pupulateSorted(arr,0,arr.length);
-    }
-
-    public void pupulateSorted(int[] arr, int start, int end){
-        if(start >= end){
-            return;
-        }
-        int mid = (start+end)/2;
-        this.insert(mid);
-        pupulateSorted(arr,start,mid);
-        pupulateSorted(arr,mid+1,end);
-    }
-
-
-    public void insert(int value){
-        root = insert(value,root);
-    }
-    public Node insert(int value, Node node){
-        if(node == null){
+    private Node insert(int value, Node node) {
+        if (node == null) {
             node = new Node(value);
             return node;
         }
 
-        if(value < node.value){
-            node.left = insert(value,node.left);
-        }else{
-            node.right = insert(value,node.right);
+        if (value < node.value) {
+            node.left = insert(value, node.left);
         }
-        node.height = Math.max(height(node.left),height(node.right))+1;
+
+        if (value > node.value) {
+            node.right = insert(value, node.right);
+        }
+
+        node.height = Math.max(height(node.left), height(node.right)) + 1;
         return rotate(node);
     }
 
-    public Node rotate(Node node){
-
-        if(node == null){
-            return node;
-        }
-//            left heavy
-        if(height(node.left) - height(node.right) > 1){
-//            left-left case
-            if(height(node.left.left) - height(node.left.right) >0){
+    private Node rotate(Node node) {
+        if (height(node.left) - height(node.right) > 1) {
+            // left heavy
+            if(height(node.left.left) - height(node.left.right) > 0) {
+                // left left case
                 return rightRotate(node);
             }
-//            left-right
-            if(height(node.left.left) - height(node.left.right) <0){
+            if(height(node.left.left) - height(node.left.right) < 0) {
+                // left right case
                 node.left = leftRotate(node.left);
                 return rightRotate(node);
             }
         }
 
-//        right heavy
-        if(height(node.left) - height(node.right) < -1){
-//            right-right case
-            if(height(node.right.left) - height(node.right.right) < 0){
+        if (height(node.left) - height(node.right) < -1) {
+            // right heavy
+            if(height(node.right.left) - height(node.right.right) < 0) {
+                // right right case
                 return leftRotate(node);
             }
-//            right-left
-            if(height(node.right.left) - height(node.right.right) >0){
+            if(height(node.right.left) - height(node.right.right) > 0) {
+                // left right case
                 node.right = rightRotate(node.right);
                 return leftRotate(node);
             }
         }
+
         return node;
     }
 
-    private Node leftRotate(Node child) {
-        Node parent = child.right;
-        Node t = parent.left;
+    public Node rightRotate(Node p) {
+        Node c = p.left;
+        Node t = c.right;
 
-        parent.left = child;
-        child.right = t;
+        c.right = p;
+        p.left = t;
 
-        parent.height = Math.max(height(parent.left),height(parent.right))+1;
-        child.height = Math.max(height(child.left),height(child.right))+1;
+        p.height = Math.max(height(p.left), height(p.right) + 1);
+        c.height = Math.max(height(c.left), height(c.right) + 1);
 
-        return parent;
+        return c;
     }
 
-    private Node rightRotate(Node parent) {
-        Node child = parent.left;
-        Node t = child.left;
+    public Node leftRotate(Node c) {
+        Node p = c.right;
+        Node t = p.left;
 
-        child.right = parent;
-        parent.left = t;
+        p.left = c;
+        c.right = t;
 
-        parent.height = Math.max(height(parent.left),height(parent.right))+1;
-        child.height = Math.max(height(child.left),height(child.right))+1;
+        p.height = Math.max(height(p.left), height(p.right) + 1);
+        c.height = Math.max(height(c.left), height(c.right) + 1);
 
-        return child;
+        return p;
     }
 
-
-    public boolean isBalanced(Node node){
-        if(node == null){
-            return true;
+    public void populate(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            this.insert(nums[i]);
         }
-        return Math.max(height(node.left),height(node.right)) <=1 && isBalanced(node.left) && isBalanced((node.right));
     }
 
-    public void display(){
-        display(root,"Root node: ");
+    public void populatedSorted(int[] nums) {
+        populatedSorted(nums, 0, nums.length);
     }
 
-    private void display(Node node, String indent) {
-        if(node == null){
+    private void populatedSorted(int[] nums, int start, int end) {
+        if (start >= end) {
             return;
         }
-        System.out.println(indent+node.value);
+
+        int mid = (start + end) / 2;
+
+        this.insert(nums[mid]);
+        populatedSorted(nums, start, mid);
+        populatedSorted(nums, mid + 1, end);
+    }
+
+    public void display() {
+        display(this.root, "Root Node: ");
+    }
+
+    private void display(Node node, String details) {
+        if (node == null) {
+            return;
+        }
+        System.out.println(details + node.value);
         display(node.left, "Left child of " + node.value + " : ");
         display(node.right, "Right child of " + node.value + " : ");
-
     }
-}
 
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    public boolean balanced() {
+        return balanced(root);
+    }
+
+    private boolean balanced(Node node) {
+        if (node == null) {
+            return true;
+        }
+        return Math.abs(height(node.left) - height(node.right)) <= 1 && balanced(node.left) && balanced(node.right);
+    }
+
+}
--- src/main/java/customs/trees/binarytree/BinarySearchTree.java
+++ src/main/java/customs/trees/binarytree/BinarySearchTree.java
@@ -64,11 +64,13 @@ public class BinarySearchTree {
             return node;
         }
 
-        if(value < node.value){
-            node.left = insert(value,node.left);
-        }else{
-            node.right = insert(value,node.right);
+        if (value < node.value) {
+            node.left = insert(value, node.left);
         }
+        if (value > node.value) {
+            node.right = insert(value, node.right);
+        }
+
         node.height = Math.max(height(node.left),height(node.right))+1;
         return node;
     }
