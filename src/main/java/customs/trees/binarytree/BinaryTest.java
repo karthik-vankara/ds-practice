32dcb44ff8ea98481e8f0b0d06261a260cadb12f
Karthik Vankara <vankara.karthik@gmail.com>
2025-02-15 14:44:48 +0530

    AVl tree

--- src/main/java/customs/trees/binarytree/BinaryTest.java
+++ src/main/java/customs/trees/binarytree/BinaryTest.java
@@ -6,9 +6,16 @@ public class BinaryTest {
 //        binaryTree.insert();
 //        binaryTree.prettyDisplay();
 
-        BinarySearchTree binarySearchTree =new BinarySearchTree();
-        int[] nums = { 5, 2, 7, 1, 4, 6, 9, 8, 3, 10 };
-        binarySearchTree.pupulate(nums);
-        binarySearchTree.display();
+//        BinarySearchTree binarySearchTree =new BinarySearchTree();
+//        int[] nums = { 5, 2, 7, 1, 4, 6, 9, 8, 3, 10 };
+//        binarySearchTree.pupulate(nums);
+//        binarySearchTree.display();
+
+        AVL avlTree = new AVL();
+        for (int i = 0; i < 1000; i++) {
+            avlTree.insert(i);
+        }
+//        Its wrong, need to check the implementation
+        System.out.println(avlTree.height());
     }
 }
