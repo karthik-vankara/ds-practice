047915e7bd16cbd16054a859ce7c7a843efdbe36
Karthik Vankara <vankara.karthik@gmail.com>
2025-02-22 14:20:14 +0530

    tree problems

---
+++ src/main/java/problems/trees/LowestCommonAnsestor.java
@@ -0,0 +1,22 @@
+package problems.trees;
+
+public class LowestCommonAnsestor {
+    public static void main(String[] args) {
+
+    }
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null){
+            return null;
+        }
+        if(root == p || root ==q){
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left,p,q);
+        TreeNode right = lowestCommonAncestor(root.right,p,q);
+
+        if(left != null && right != null){
+            return root;
+        }
+        return left == null ? right : left;
+    }
+}
